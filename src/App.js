8f64628269153562000e12774a684f42c14404d5
Dheeraj Bisht <dheerajbisht876@gmail.com>
2022-09-22 01:58:49 +0530

    app created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import MobileDetect from "mobile-detect";
+import {
+  RotatingSquare,
+  CirclesWithBar,
+  Audio,
+  BallTriangle,
+  Bars,
+} from "react-loader-spinner";
 
-function App() {
+export default function App() {
+  const redirect = (url) => {
+    window.location = url;
+  };
+
+  React.useEffect(() => {
+    const md = new MobileDetect(window.navigator.userAgent);
+    if (md.os() === "AndroidOS")
+      setTimeout(
+        () =>
+          redirect(
+            "https://play.google.com/store/apps/details?id=com.whatsapp"
+          ),
+        500
+      );
+    else if (md.is("iPhone"))
+      setTimeout(
+        () =>
+          redirect(
+            "https://apps.apple.com/in/app/whatsapp-messenger/id310633997"
+          ),
+        500
+      );
+    else setTimeout(() => redirect("https://jhumoradio.com/"), 500);
+  }, []);
+
+  const spinners = [
+    <RotatingSquare
+      height="100"
+      width="100"
+      color="#f0ea41"
+      ariaLabel="rotating-square-loading"
+      strokeWidth="4"
+      wrapperStyle={{}}
+      wrapperClass=""
+      visible={true}
+    />,
+    <CirclesWithBar
+      height="100"
+      width="100"
+      color="#f0ea41"
+      ariaLabel="rotating-square-loading"
+      strokeWidth="4"
+      wrapperStyle={{}}
+      wrapperClass=""
+      visible={true}
+    />,
+    <Audio
+      height="100"
+      width="100"
+      color="#f0ea41"
+      ariaLabel="rotating-square-loading"
+      strokeWidth="4"
+      wrapperStyle={{}}
+      wrapperClass=""
+      visible={true}
+    />,
+    <BallTriangle
+      height="100"
+      width="100"
+      color="#f0ea41"
+      ariaLabel="rotating-square-loading"
+      strokeWidth="4"
+      wrapperStyle={{}}
+      wrapperClass=""
+      visible={true}
+    />,
+    <Bars
+      height="100"
+      width="100"
+      color="#f0ea41"
+      ariaLabel="rotating-square-loading"
+      strokeWidth="4"
+      wrapperStyle={{}}
+      wrapperClass=""
+      visible={true}
+    />,
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {spinners[Math.floor(Math.random() * spinners.length)]}
     </div>
   );
 }
-
-export default App;
